2035c77b46cb0b85e58f074e891aa92f1c0e038d
ombeltran <ombeltranduk@gmail.com>
2025-01-21 08:17:57 -0600

    Adjusting display in about and contact section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,7 +18,7 @@ export const About = () => {
         }
     ]
     return (
-        <div className="2xl:h-screen h-fit bg-gradient-to-t from-red-200 to-white">
+        <div className="abouth-fit bg-gradient-to-t from-red-200 to-white">
             <div className="h-[72px]"></div>
             <Section className="h-full">
                 <div className="flex flex-row mx-auto items-center p-4 text-xl w-[92%] gap-4 ">
